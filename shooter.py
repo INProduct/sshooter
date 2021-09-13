27f152e0d8e1166980a21d79fa64de6aa77bd2e6
alex <a.jurcenko@inproduct.de>
2021-09-13 19:41:57 +0200

    First Functionality

--- shooter.py
+++ shooter.py
@@ -1,4 +1,11 @@
 from kivy.app import App
+from kivy.core.window import Window
+import sys
+
+if sys.platform.startswith('linux'):
+    import pyscreenshot as ImageGrab
+else:
+    from PIL import ImageGrab
 
 
 class ShooterApp(App):
@@ -6,6 +13,26 @@ class ShooterApp(App):
     def build(self):
         pass
 
+    def shoot(self):
+        print(self.root.ids["'x_pos'"].text)
+        props = [self.root.ids["'x_pos'"].text, self.root.ids["'y_pos'"].text, self.root.ids["'width'"].text, self.root.ids["'height'"].text]
+        numbs = []
+        for p in props:
+            try:
+                n = int(p)
+            except:
+                n = None
+            numbs.append(n)
+        if all(numbs):
+            im = ImageGrab.grab(bbox=tuple(numbs))
+        else:
+            im = ImageGrab.grab()
+        im.show()
+
 
 if __name__ == '__main__':
-    app = ShooterApp().run()
+    app = ShooterApp()
+    app.title = 'INProduct ShooteR'
+
+    Window.size = (500, 100)
+    app.run()
