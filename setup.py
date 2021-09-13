bee6a3783bfc3f22bf82b8623971f23084d3ea00
alex <a.jurcenko@inproduct.de>
2021-09-13 19:48:24 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sshooter',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='alex',
+    author_email='info@inproduct.de',
+    description='screencapture'
+)
